7d9367531b5d30bfd2f69efd60d75452f97cee0f
Sid <sidreds06@gmail.com>
2021-05-29 21:15:11 +0530

    port

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.static(__dirname));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 // Start the server
 app.listen(PORT, function(){
