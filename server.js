7715fb51e3d74c97e8f2815119c3723abbddacf4
Sid <sidreds06@gmail.com>
2021-05-29 20:29:46 +0530

    one

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.static(__dirname));
+
+app.get("/", function(req, res){
+    res.sendFile('index.html', {root: __dirname});
+    //res.send("Welcome to My Basic Site");  
+})
+const PORT = 5000;
+
+// Start the server
+app.listen(PORT, function(){
+    console.log("Server Starting running on http://localhost:"+PORT);
+})
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
