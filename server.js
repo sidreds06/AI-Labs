095955661ecb96ae1091e77d1cf2069af97409b0
Sid <sidreds06@gmail.com>
2021-06-10 18:29:48 +0530

    auth

--- server.js
+++ server.js
@@ -3,12 +3,23 @@ const DB = require('../AI-Labs/database/connection')
 var router = express.Router()
 const upload = require('express-fileupload');
 const { render } = require('ejs');
+const session = require('express-session')
+var bodyParser = require('body-parser')
+var MySQLStore = require('express-mysql-session')(session);
+
 
 const app = express();
 
-var SqlString = require('sqlstring');
+var options = {
+	host     : 'localhost',
+    user     : 'root',
+    password : '',
+    database : 'projects'
+};
 
-app.use('/', router)
+var sessionStore = new MySQLStore(options);
+
+var SqlString = require('sqlstring');
 
 app.use(upload())
 
@@ -19,6 +30,30 @@ app.use(express.static(__dirname));
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+
+
+app.use(session({
+    name: 'sid',
+    resave: false,
+    saveUninitialized: false,
+    secret: 'blackbuck123',
+     cookie: {
+         maxAge: 1000*60*60,
+         sameSite: true,
+         secure: false
+     },
+     store: sessionStore
+}))
+
+var isAuth=(req,res,next)=>{
+   if(req.session.userId)
+     next()
+   else
+     res.redirect('login')  
+}
+
+app.use('/', router)
+
 
 const PORT = process.env.PORT || 5000;
 
@@ -32,7 +67,8 @@ app.get("/home", function(req, res){
     res.render('homepage')
 })
 
-app.get("/projects", function(req, res){
+app.get("/projects",isAuth, function(req, res){
+    console.log(req.session.userId)
     res.render('projects')
 })
 
@@ -44,7 +80,7 @@ app.get("/my-projects", function(req, res){
         else{
         res.render('myprojects',{data})
          }
-      
+
     })
 })
 
@@ -53,7 +89,7 @@ app.get("/add-projects", function(req, res){
 })
 
 app.post("/create", function(req, res){
-  
+
     if(req.files){
         var file =req.files.image
         var filename = file.name
@@ -74,14 +110,16 @@ app.post("/create", function(req, res){
             else{
             res.render('myprojects',{data})
              }
-          
+
         })
       }
     });
   });
 
 
-router.get("/info", function(req, res){
+app.get("/info",isAuth, function(req, res){
+    req.session.userId = 1
+    console.log(req.session)
     DB.query(`SELECT * FROM list`, (err,data) =>{
         if(err){
             console.log(err)
@@ -89,9 +127,9 @@ router.get("/info", function(req, res){
         else{
         res.render('homepage',{data})
          }
-      
+
     })
-   
+
 })
 
 app.get("/delete/:id", function(req, res){
@@ -108,7 +146,7 @@ app.get("/delete/:id", function(req, res){
                 else{
                 res.render('myprojects',{data})
                  }
-              
+
             })
         }
 
@@ -146,14 +184,14 @@ app.post("/update/:id", function(req, res){
                 else{
                 res.render('myprojects',{data})
                  }
-              
+
             })
 
         }
     })
 
 
-})    
+})
 
 app.get("/view/:id", function(req, res){
 
@@ -168,5 +206,50 @@ app.get("/view/:id", function(req, res){
     })
 
 })
+
+app.get("/register", function(req, res){
+    res.render('register')
+})
+
+app.get("/login", function(req, res){
+    res.render('login')
+})
+
+app.post("/register", function(req, res){
+
+    var user=req.body
+    //var sql = `INSERT INTO user (name, username, email, password) VALUES (${SqlString.escape(user.name)}, ${SqlString.escape(user.username)}, ${SqlString.escape(user.email)}, ${SqlString.escape(user.password)})`
+    var sql = `INSERT INTO user (name, username, email, password) VALUES ('${user.name}', '${user.username}', '${user.email}', '${user.password}')`
+    DB.query(sql, function (err, result) {
+      if (err) throw err;
+      else{
+       res.redirect('login')
+         }
+
+        })    
+  })
+
+  app.post("/login", function(req, res){
+      const user = req.body
+    DB.query(`SELECT * FROM user WHERE email = "${user.uname}" AND password = "${user.psd}"`, (err,data) =>{
+             if(data.length > 0){
+            req.session.userId = data[0].id    
+            //res.redirect('projects')
+            res.json(req.session.userId)
+        }
+        else{
+            res.redirect('login')
+         }
+    })
+})
+
+app.get('/logout', function(req, res){
+    req.session.destroy((err)=>{
+        if(err) throw err;
+        res.redirect('login')
+    
+    })
+})  
+
 
 module.exports = router
